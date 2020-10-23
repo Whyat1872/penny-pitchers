70eb0a64b76dcab8aa6a7fab23afe5ba28e9ee84
Wyatt <tidus1872@hotmail.com>
2020-10-22 22:40:31 -0700

    Environment tilemap work

--- EnemySpawnArea.gd
+++ EnemySpawnArea.gd
@@ -21,7 +21,4 @@ func _on_SpawnTimer_timeout():
 	var direction = $EnemySpawnSpot.rotation + PI / 2
 	mob.position = $EnemySpawnSpot.position
 	direction += rand_range(-PI / 4, PI / 4)
-#	mob.rotation = direction
-#	mob.linear_velocity = Vector2(rand_range(mob.min_speed, mob.max_speed), 0).rotated(direction)
-	# warning-ignore:return_value_discarded
-#	$HUD.connect("start_game", mob, "_on_start_game")
+	
